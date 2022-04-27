db965068ccc1ff3f5d4c6ed275484921fe1722fa
Dylan <dylanoelke.business@gmail.com>
2022-04-27 13:53:26 -0500

    Update Zombie.cs

--- MAGD 488 TCG/Assets/Scripts/Cards/Creature/Card Scripts/Hunger/Zombie.cs
+++ MAGD 488 TCG/Assets/Scripts/Cards/Creature/Card Scripts/Hunger/Zombie.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "New Creature/Zombie", fileName = "Card")]
 public class Zombie : Creature
 {
+    public Creature zombieToken;
+    [SerializeField] int abilityCost;
     public override void OnAttack(Tile[] hostBoard, Tile[] clientBoard, Tile attacker, bool isHost, Tile attacked)
     {
         base.OnAttack(hostBoard, clientBoard, attacker, isHost, attacked);
@@ -12,5 +14,11 @@ public class Zombie : Creature
     public override void OnAbility(Tile[] hostBoard, Tile[] clientBoard, Tile attacker, bool isHost)
     {
         //Create zombie token in hand with 1/1/0 - 2 mana
+        if(GameManager.Singleton.currentMana >= abilityCost)
+        {
+            GameManager.Singleton.myHand.AddCardToHand(zombieToken);
+            GameManager.Singleton.AffectCurrentMana(-abilityCost);
+        }
+            
     }
 }
