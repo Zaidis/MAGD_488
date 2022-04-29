d9bb608774b37daed50fa0b977f81584ddfc94da
djw191 <50028101+djw191@users.noreply.github.com>
2022-04-29 16:08:06 -0500

    Fixed Matchmaking bug on subsequent matches

--- MAGD 488 TCG/Assets/Scripts/GameManager.cs
+++ MAGD 488 TCG/Assets/Scripts/GameManager.cs
@@ -152,11 +152,13 @@ public class GameManager : MonoBehaviour
         clientHealthText.text = clientHealth.ToString();
 
         if(hostHealth <= 0) {    //Check for eitherplayer death, load respective scene on either with a safe, delayed, scene change
+            MythosClient.instance.OnOutcome(false);
             if (isHost)
                 StartCoroutine(SafeSceneChange(2, loseScene));
             else
                 StartCoroutine(SafeSceneChange(2, winScene));
-        } else if(clientHealth <= 0) {       
+        } else if(clientHealth <= 0) {
+            MythosClient.instance.OnOutcome(true);
             if (!isHost)
                 StartCoroutine(SafeSceneChange(2, loseScene));
             else
