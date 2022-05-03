beacbd6757ca863426a55028d697a8ff5e0b3524
Zaidis <chewybinx@gmail.com>
2022-05-03 01:42:11 -0500

    Tooltip + UI attributes

---
+++ MAGD 488 TCG/Assets/Scripts/Tooltip.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class Tooltip : MonoBehaviour
+{
+
+
+    public TextMeshProUGUI title, description;
+
+
+}
